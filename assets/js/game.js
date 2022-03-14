fd60d567df1c705200249f5a9f8c351e2d30347e
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-03-14 18:56:26 -0400

    added multiple enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,13 +2,22 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-var confirmSkip =true;
-//You can also log multiple values at once like this 
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
 var enemyHealth = 50;
 var enemyAttack = 12;
+
 // fight function
 var fight = function() {
 // Alert players that they are starting the round
@@ -63,4 +72,4 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 };
 
 // run fight function to start game
-fight();
+//fight()
